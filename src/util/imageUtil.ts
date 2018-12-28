759712694b319e1ecd560e1a1eab636f3208d4b6
Gabriel <gabrielchiquini@hotmail.com>
2018-12-28 19:40:33 -0200

    Fixed tslint warning

--- src/util/imageUtil.ts
+++ src/util/imageUtil.ts
@@ -4,7 +4,7 @@ export function getImage(path: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const image = new Image();
     image.onload = () => resolve(image);
-    image.onerror = (event) => reject(event);
+    image.onerror = event => reject(event);
     image.src = path;
   });
 }
