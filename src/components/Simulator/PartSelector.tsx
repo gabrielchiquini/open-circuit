e4df1ad1b4c5a4fd25fccc716aeaff2cbc7f7557
Gabriel Santos Chiquini <gabriel@localhost.localdomain>
2018-12-28 01:44:41 -0200

    Part Selector for circuit

---
+++ src/components/Simulator/PartSelector.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class PartSelector extends Component {
+  render() {
+    return (
+      <div className="circuit-parts">
+      </div>
+    );
+  }
+}
