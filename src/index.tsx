ea9b6e25a77a841d9a9be9c6cc95c2a1cd5c4bdb
Gabriel Santos Chiquini <gabriel@localhost.localdomain>
2018-12-28 01:43:54 -0200

    Modified to support store

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SimulatorContainer from './components/Simulator/SimulatorContainer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-
+const store = createStore((state: any, action) => state);
 const startApp = () => {
-  ReactDOM.render( <App />
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <Route path="/" exact component={SimulatorContainer} />
+      </Router>
+    </Provider>
     , document.getElementById('root'));
-}
+};
 
-if(!(window as any).cordova) {
+if (!(window as any).cordova) {
   startApp();
 } else {
   document.addEventListener('deviceready', startApp, false);
