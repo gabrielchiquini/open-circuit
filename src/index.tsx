bec0f69280d870a53c9e55a67a1059063abbc438
Gabriel <gabrielchiquini@hotmail.com>
2018-12-28 19:42:11 -0200

    Removed redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SimulatorContainer from './components/Simulator/SimulatorContainer';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-const store = createStore((state: any, action) => state);
 const startApp = () => {
   ReactDOM.render(
-    <Provider store={store}>
       <Router>
         <Route path="/" exact component={SimulatorContainer} />
       </Router>
-    </Provider>
     , document.getElementById('root'));
 };
 
