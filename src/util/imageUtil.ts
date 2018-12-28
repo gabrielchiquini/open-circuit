95c18be16c176ecf65c96aeabe7d19f5e90693e8
Gabriel Santos Chiquini <gabriel@localhost.localdomain>
2018-12-28 01:35:04 -0200

    Function to retrieve image node as Promise

---
+++ src/util/imageUtil.ts
@@ -0,0 +1,10 @@
+export const CIRCUIT_MESH = 'assets/circuit/mesh.svg';
+
+export function getImage(path: string): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.onload = () => resolve(image);
+    image.onerror = (event) => reject(event);
+    image.src = path;
+  })
+}
