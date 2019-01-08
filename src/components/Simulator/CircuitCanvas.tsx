8fefd3c99861235db06eb2a93ef5ad9888721d6d
Gabriel <gabrielchiquini@hotmail.com>
2019-01-07 23:57:17 -0200

    Circuit now is capable of add nodes and connections

--- src/components/Simulator/CircuitCanvas.tsx
+++ src/components/Simulator/CircuitCanvas.tsx
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
-import Konva, { KonvaEventObject } from 'konva';
-import { CIRCUIT_MESH, getImage } from "../../util/imageUtil";
+import Konva, { KonvaEventObject } from "konva";
+import { getImage } from "../../util/imageUtil";
 import { SomePart } from "./SimulatorContainer";
 import Circuit from "./Circuit/Circuit";
+import {
+  CIRCUIT_MESH,
+  calcGroupDimension,
+  ensureInside,
+  calculateCenter,
+  CIRCUIT_COLOR,
+} from "./Circuit/util";
+import PartName from "./Circuit/Parts/PartName";
 
-interface IProps { circuit: Circuit; selectedElement: SomePart; }
+interface IProps {
+  circuit: Circuit;
+  selectedElement: SomePart;
+}
 
 export default class CircuitCanvas extends Component<IProps> {
-  canvas: Konva.Stage = null;
   circuitLayer: Konva.Layer = null;
   circuit: Circuit;
+  private canvas: Konva.Stage = null;
+  private selectedPole: Konva.Circle;
 
   constructor(props: Readonly<IProps>) {
     super(props);
@@ -17,20 +29,17 @@ export default class CircuitCanvas extends Component<IProps> {
   }
 
   render() {
-    return (
-      <div className="circuit-area" id="circuit-area"></div>
-    );
+    return <div className="circuit-area" id="circuit-area" />;
   }
   componentDidMount() {
     this.setupKonva();
   }
 
-
   async setupKonva(): Promise<void> {
     this.canvas = new Konva.Stage({
-      container: 'circuit-area',
-      width: 1200,
-      height: 1200,
+      container: "circuit-area",
+      width: 600,
+      height: 600,
     });
     const baseLayer = new Konva.Layer();
     this.setupCircuitLayer();
@@ -44,20 +53,84 @@ export default class CircuitCanvas extends Component<IProps> {
     this.circuitLayer = new Konva.Layer();
   }
 
-  addSelectedElement(ev: KonvaEventObject<MouseEvent>): void {
+  async addSelectedElement(ev: KonvaEventObject<Event>): Promise<void> {
+    const { posX, posY } = this.guessClickPosition(ev);
     const part = new this.props.selectedElement();
+    const node = await part.konvaNode;
+    this.setupPartPosition(node, posX, posY);
+    this.addEvents(node);
     this.circuit.addPart(part);
-    const node = new Konva.Shape({
-      id: part.id,
-      x: ev.evt.layerX,
-      y: ev.evt.layerY,
-    });
     this.addNode(node);
+  }
+  addEvents(node: Konva.Group): any {
+    node.getChildren().each(childNode => {
+      if (childNode.name() === PartName.Pole) {
+        childNode.on("click touchend", ev => {
+          this.handlePoleClick(ev);
+        });
+      }
+    });
+  }
+
+  private handlePoleClick(ev: Konva.KonvaEventObject<Event>) {
+    const target = ev.target as Konva.Circle;
+    if (this.selectedPole) {
+      this.circuit.addConnection(this.selectedPole.id(), target.id());
+      const position1 = this.selectedPole.getAbsolutePosition();
+      const position2 = target.getAbsolutePosition();
+      const line = new Konva.Line({
+        points: [
+          position1.x,
+          position1.y,
+          position2.x,
+          position2.y,
+        ],
+        stroke: CIRCUIT_COLOR,
+        strokeWidth: 1,
+        tension: 1,
+      });
+      this.selectedPole.fill(CIRCUIT_COLOR);
+      target.fill(CIRCUIT_COLOR);
+      this.addNode(line);
+      this.selectedPole = null;
+    } else {
+      this.selectedPole = target;
+      target.fill("red");
+      target.draw();
+    }
+  }
+
+  private setupPartPosition(node: Konva.Group, posX: number, posY: number) {
+    const groupDimension = calcGroupDimension(node);
+    const xPosition = ensureInside(
+      calculateCenter(groupDimension.width, posX),
+      this.width,
+    );
+    const yPosition = ensureInside(
+      calculateCenter(groupDimension.height, posY),
+      this.heigth,
+    );
+    node.x(xPosition);
+    node.y(yPosition);
+  }
+
+  private guessClickPosition(ev: Konva.KonvaEventObject<Event>) {
+    let posX = 0;
+    let posY = 0;
+    if (ev.evt instanceof TouchEvent) {
+      const touch = ev.evt.changedTouches[0];
+      posY = touch.clientY - this.canvas.container().offsetTop;
+      posX = touch.clientX - this.canvas.container().offsetLeft;
+    } else if (ev.evt instanceof MouseEvent) {
+      posY = ev.evt.layerY;
+      posX = ev.evt.layerX;
+    }
+    return { posX, posY };
   }
 
   private addNode(node: Konva.Node) {
     this.circuitLayer.add(node);
-    this.canvas.draw();
+    this.canvas.batchDraw();
   }
 
   private async setupBackground(baseLayer: Konva.Layer) {
@@ -66,9 +139,17 @@ export default class CircuitCanvas extends Component<IProps> {
       width: baseLayer.width(),
       height: baseLayer.height(),
       fillPatternImage: meshImage,
-      fillPatternRepeat: 'repeat',
+      fillPatternRepeat: "repeat",
     });
     baseLayer.add(background);
-    baseLayer.on('click', ev => this.addSelectedElement(ev));
+    baseLayer.on("click touchend", ev => this.addSelectedElement(ev));
+  }
+
+  private get width(): number {
+    return this.canvas.width();
+  }
+
+  private get heigth(): number {
+    return this.canvas.height();
   }
 }
