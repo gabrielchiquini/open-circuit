c61f677609d4803ec8f2d9fe93c616157477b624
Gabriel <gabrielchiquini@hotmail.com>
2019-01-23 23:22:03 -0200

    New dependencies
    Bootstrap to correct CSS
    node-sass to preprocess CSS
    fontaewsome for icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SimulatorContainer from './components/Simulator/SimulatorContainer';
 
